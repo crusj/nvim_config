391d4b30db44ce0427a8336e8a08258bed0e37f9
蓝天武士 <296769589@qq.com>
2022-04-16 16:56:52 +0800

    golangci-lint --enable-all false

--- lua/quickfix.lua
+++ lua/quickfix.lua
@@ -1,6 +1,5 @@
 -- golangcilint
 vim.cmd([[
-
 	let g:asyncrun_open = 8
-	nmap <silent> gl <cmd>AsyncRun -strip golangci-lint run --issues-exit-code=0 --enable "gofumpt,nlreturn,whitespace,stylecheck,structcheck,unused,errname,forcetypeassert,goconst,gocyclo,godox,ifshort,makezero,tagliatelle,unparam,wsl,gomnd,errchkjson" "$(VIM_FILEPATH)"<cr>
+	nmap <silent> gl <cmd>AsyncRun golangci-lint run  --fast --issues-exit-code=0 --enable-all  "$(VIM_FILEPATH)"<cr>
 ]])
--- lua/tools_setup.lua
+++ lua/tools_setup.lua
@@ -1,7 +1,6 @@
 -- translate plugin
 vim.cmd([[
 	let g:translator_default_engines = ['google']
-	let g:translator_proxy_url = 'socks5://127.0.0.1:1086'
 	nmap <silent> <Leader>w <Plug>TranslateW
 	vmap <silent> <Leader>w <Plug>TranslateWV
 ]])
