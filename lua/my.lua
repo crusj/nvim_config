838b6aac37eedada0e9678839040c0528995259b
jianglong <jianglong199212@gmail.com>
2022-05-06 20:14:33 +0800

    Generate go tags add notify

--- lua/my.lua
+++ lua/my.lua
@@ -5,15 +5,32 @@ vim.cmd([[
 highlight NotifyWARNIcon guifg=Green
 highlight NotifyINFOBorder guifg=Green
 highlight NotifyINFOTitle guifg=Green
+highlight NotifyINFOIcon guifg=Green
 ]])
 
+-- generate go tags for project
 function my.generate_go_tags()
 	local cwd = vim.fn.getcwd()
-	local cmd = "rm tags && gotags -R " .. cwd .. " > tags"
+	if not vim.loop.fs_stat(cwd .. "/go.mod") then
+		notify.notify("Do not found go.mod", "error", {
+			title = "Generate go tags",
+		})
+		return
+	end
+
+	local cmd = "gotags -R " .. cwd .. " > " .. cwd .. "/tags"
+	if vim.loop.fs_stat(cwd .. "/tags") then
+		cmd = "rm " .. cwd .. "/tags && " .. cmd
+	end
+
 	vim.fn.jobstart(cmd, {
 		on_exit = function()
 			if vim.loop.fs_stat(cwd .. "/tags") then
 				notify.notify("Success", "info", {
+					title = "Generate go tags",
+				})
+			else
+				notify.notify("Error", "error", {
 					title = "Generate go tags",
 				})
 			end
