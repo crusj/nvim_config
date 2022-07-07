281bd29af87487936353c356d64990a535e31346
crusj <jianglong199212@gmail.com>
2022-07-07 16:17:31 +0800

    New quick copy

--- lua/my.lua
+++ lua/my.lua
@@ -43,17 +43,28 @@ function my.generate_go_tags()
     })
 end
 
+-- 关闭最近win
 function my.close_last_git_win()
     if my.git_log_win ~= nil and vim.api.nvim_win_is_valid(my.git_log_win) then
         vim.api.nvim_win_close(my.git_log_win, true)
     end
 end
 
+-- 选择log
+function my.choose_git_log()
+    vim.fn.execute("normal! Vy")
+    my.close_last_git_win()
+end
+
 -- copy 当前项目最近git log body信息
 function my.copy_last_git_msg()
     if my.git_log_buf == nil then
         my.git_log_buf = vim.api.nvim_create_buf(false, true)
+
+        -- bind buf keymap
         vim.api.nvim_buf_set_keymap(my.git_log_buf, "n", "<esc>", ":lua require'my'.close_last_git_win()<CR>",
+            { silent = true })
+        vim.api.nvim_buf_set_keymap(my.git_log_buf, "n", "<cr>", ":lua require'my'.choose_git_log()<CR>",
             { silent = true })
     end
 
@@ -68,6 +79,7 @@ function my.copy_last_git_msg()
             end
         end,
         on_exit = function()
+            -- 计算高度
             local ew = vim.api.nvim_get_option("columns")
             local eh = vim.api.nvim_get_option("lines")
             local gw = math.floor(ew * 0.7)
@@ -81,7 +93,7 @@ function my.copy_last_git_msg()
                 border = "double",
             })
 
-	        vim.api.nvim_win_set_option(my.git_log_win, "winhighlight", "CursorLine:hl_git_log_csl")
+            vim.api.nvim_win_set_option(my.git_log_win, "winhighlight", "CursorLine:hl_git_log_csl")
             vim.api.nvim_buf_set_lines(my.git_log_buf, 0, #lines, false, lines)
         end
     })
